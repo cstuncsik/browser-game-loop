54062220dd76531c2a7c33c7361e5d1764d1cd05
Csaba Tuncsik <cstuncsik@cstuncsiks-MBP.local>
2017-03-31 10:28:23 +0200

    fix($linter): fixing bithuound linting

--- examples/main.js
+++ examples/main.js
@@ -1,48 +1,48 @@
 requirejs(['../src/gameLoop'], function (createLoop) {
 
-    var box = document.getElementById('box'),
-        prevBoxRotation = 0,
-        currBoxRotation = 0,
-        velocity = 0.05,
-        direction = 1,
-        mouseDown = false,
-        fpsDisplay = document.getElementById('fpsDisplay'),
-        elapsedTimeDisplay = document.getElementById('elapsedTimeDisplay');
+  var box = document.getElementById('box'),
+    prevBoxRotation = 0,
+    currBoxRotation = 0,
+    velocity = 0.05,
+    direction = 1,
+    mouseDown = false,
+    fpsDisplay = document.getElementById('fpsDisplay'),
+    elapsedTimeDisplay = document.getElementById('elapsedTimeDisplay');
 
-    var loop = createLoop({
-        updateTimeStep: 1000 / 60,
-        input: input,
-        update: update,
-        render: render
-    });
+  var loop = createLoop({
+    updateTimeStep: 1000 / 60,
+    input: input,
+    update: update,
+    render: render
+  });
 
-    document.addEventListener('mousedown', function () {
-        mouseDown = true;
-    });
+  document.addEventListener('mousedown', function () {
+    mouseDown = true;
+  });
 
-    document.addEventListener('mouseup', function () {
-        mouseDown = false;
-    });
+  document.addEventListener('mouseup', function () {
+    mouseDown = false;
+  });
 
-    function input() {
-        if (mouseDown) {
-            loop.setSlow(3);
-        } else {
-            loop.setSlow(1);
-        }
+  function input() {
+    if (mouseDown) {
+      loop.setSlow(3);
+    } else {
+      loop.setSlow(1);
     }
+  }
 
-    function update(step) {
-        prevBoxRotation = currBoxRotation;
-        currBoxRotation += step * velocity * direction;
-    }
+  function update(step) {
+    prevBoxRotation = currBoxRotation;
+    currBoxRotation += step * velocity * direction;
+  }
 
-    function render(interp) {
-        var interpolatedBoxRotation = (currBoxRotation * interp) + (prevBoxRotation * (1 - interp));
-        box.style.transform = 'rotate(' + interpolatedBoxRotation + 'deg)';
-        fpsDisplay.textContent = Math.round(loop.getFps()) + ' FPS';
-        elapsedTimeDisplay.textContent = Math.round(loop.getElapsedTime()) + ' s';
-    }
+  function render(interp) {
+    var interpolatedBoxRotation = (currBoxRotation * interp) + (prevBoxRotation * (1 - interp));
+    box.style.transform = 'rotate(' + interpolatedBoxRotation + 'deg)';
+    fpsDisplay.textContent = Math.round(loop.getFps()) + ' FPS';
+    elapsedTimeDisplay.textContent = Math.round(loop.getElapsedTime()) + ' s';
+  }
 
-    loop.start();
+  loop.start();
 });
--- src/gameLoop.js
+++ src/gameLoop.js
@@ -1,86 +1,86 @@
 (function (factory) {
-    'use strict';
-    if (typeof define === 'function' && define.amd) {
-        define([], factory);
-    }
-    else if (typeof exports === 'object') {
-        module.exports = factory();
-    }
-    else {
-        window.createGameLoop = factory();
-    }
+  'use strict';
+  if (typeof define === 'function' && define.amd) {
+    define([], factory);
+  }
+  else if (typeof exports === 'object') {
+    module.exports = factory();
+  }
+  else {
+    window.createGameLoop = factory();
+  }
 }(function () {
-    return function (options) {
-        'use strict';
-        var sec = 1000,
-            updateTimeStep = options.updateTimeStep || sec / 30,
-            delta = 0,
-            lag = 0,
-            now = performance.now(),
-            then = now,
-            beginning = then,
-            fpsFilterStrength = options.fpsFilterStrength || 20,
-            frameTime = 0,
-            first = false,
-            slow = options.slow || 1,
-            slowStep = slow * updateTimeStep,
-            update = options.update,
-            render = options.render,
-            input = options.input,
-            rafId;
+  return function (options) {
+    'use strict';
+    var sec = 1000,
+      updateTimeStep = options.updateTimeStep || sec / 30,
+      delta = 0,
+      lag = 0,
+      now = performance.now(),
+      then = now,
+      beginning = then,
+      fpsFilterStrength = options.fpsFilterStrength || 20,
+      frameTime = 0,
+      first = false,
+      slow = options.slow || 1,
+      slowStep = slow * updateTimeStep,
+      update = options.update,
+      render = options.render,
+      input = options.input,
+      rafId;
 
-        function frame() {
-            rafId = requestAnimationFrame(frame);
-            input();
-            now = performance.now();
-            delta = now - then;
-            then = now;
-            lag += Math.min(sec, delta);
-            while (lag >= slowStep) {
-                lag -= slowStep;
-                update(updateTimeStep);
-            }
-            frameTime += (delta - frameTime) / fpsFilterStrength;
-            render(lag / slowStep);
-        }
-
-        function start() {
-            then = performance.now();
-            if (!first) {
-                first = true;
-                beginning = then;
-            }
-            rafId = requestAnimationFrame(frame);
-        }
-
-        function stop() {
-            cancelAnimationFrame(rafId);
-        }
-
-        function getFps() {
-            return sec / frameTime;
-        }
-
-        function getElapsedTime() {
-            return (then - beginning) / sec;
-        }
-
-        function setSlow(s) {
-            slow = s;
-            slowStep = slow * updateTimeStep;
-        }
-
-        function getSlow() {
-            return slow;
-        }
-
-        return {
-            start: start,
-            stop: stop,
-            getFps: getFps,
-            getElapsedTime: getElapsedTime,
-            setSlow: setSlow,
-            getSlow: getSlow
-        };
+    function frame() {
+      rafId = requestAnimationFrame(frame);
+      input();
+      now = performance.now();
+      delta = now - then;
+      then = now;
+      lag += Math.min(sec, delta);
+      while (lag >= slowStep) {
+        lag -= slowStep;
+        update(updateTimeStep);
+      }
+      frameTime += (delta - frameTime) / fpsFilterStrength;
+      render(lag / slowStep);
     }
+
+    function start() {
+      then = performance.now();
+      if (!first) {
+        first = true;
+        beginning = then;
+      }
+      rafId = requestAnimationFrame(frame);
+    }
+
+    function stop() {
+      cancelAnimationFrame(rafId);
+    }
+
+    function getFps() {
+      return sec / frameTime;
+    }
+
+    function getElapsedTime() {
+      return (then - beginning) / sec;
+    }
+
+    function setSlow(s) {
+      slow = s;
+      slowStep = slow * updateTimeStep;
+    }
+
+    function getSlow() {
+      return slow;
+    }
+
+    return {
+      start: start,
+      stop: stop,
+      getFps: getFps,
+      getElapsedTime: getElapsedTime,
+      setSlow: setSlow,
+      getSlow: getSlow
+    };
+  };
 }));
